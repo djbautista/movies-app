229e6fa33f35debf27579f83b571ef753e05b4e6
David Bautista <David.Bautista.-ND@disney.com>
2024-12-17 18:29:55 -0500

    refactor(scss-migration): Migrated button component

--- components/Button/Button.test.tsx
+++ components/Button/Button.test.tsx
@@ -1,8 +1,7 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 import { screen, render, cleanup } from '@testing-library/react';
-
-import Button, { ButtonProps, getButtonClassName, Variant } from './Button';
-import { twMerge } from 'tailwind-merge';
+import Button, { ButtonProps, Variant } from './Button';
+import styles from './button.module.scss';
 
 const setup = (props: ButtonProps = {}) =>
   render(<Button {...props} data-testid="button" />);
@@ -10,79 +9,29 @@ const setup = (props: ButtonProps = {}) =>
 describe('Button', () => {
   beforeEach(cleanup);
 
-  const baseClassNames = ['w-full', 'rounded-sm', 'px-6 py-4', 'font-medium'];
-
-  const variantClassNames = {
-    primary: [
-      'bg-primary',
-      'text-white',
-      'hover:bg-primary-600',
-      'active:bg-primary-700',
-      'transition-colors',
-      'duration-200',
-    ],
-    neutral: [
-      'bg-neutral-50',
-      'text-neutral',
-      'hover:bg-neutral-100',
-      'active:bg-neutral-200',
-      'transition-colors',
-      'duration-200',
-    ],
-    ghost: [
-      'bg-transparent',
-      'text-primary',
-      'hover:bg-primary-50',
-      'active:bg-primary-100',
-      'transition-colors',
-      'duration-200',
-    ],
-  };
-
   it('renders Button component with the default props', () => {
-    const expectedClasses = [
-      ...baseClassNames,
-      ...variantClassNames['primary'],
-    ];
-
     setup();
     const button = screen.getByTestId('button');
-    const generatedClasses = getButtonClassName();
-
-    expect(generatedClasses).toBe(twMerge(expectedClasses));
-    expect(button.className).toBe(generatedClasses);
+    expect(button).toHaveClass(styles.button, styles['button--primary']);
   });
 
   it.each(['primary', 'neutral', 'ghost'] as Variant[])(
     `renders Button component with %s variant`,
     (variant) => {
-      const expectedClasses = [
-        ...baseClassNames,
-        ...variantClassNames[variant],
-      ];
-
       setup({ variant });
       const button = screen.getByTestId('button');
-      const generatedClasses = getButtonClassName({ variant });
-
-      expect(generatedClasses).toBe(twMerge(expectedClasses));
-      expect(button.className).toBe(generatedClasses);
+      expect(button).toHaveClass(styles.button, styles[`button--${variant}`]);
     },
   );
 
   it('renders Button component with custom className', () => {
     const customClassName = 'custom-class';
-    const expectedClasses = [
-      ...baseClassNames,
-      ...variantClassNames['primary'],
-      customClassName,
-    ];
-
     setup({ className: customClassName });
     const button = screen.getByTestId('button');
-    const generatedClasses = getButtonClassName({ className: customClassName });
-
-    expect(generatedClasses).toBe(twMerge(expectedClasses));
-    expect(button.className).toBe(generatedClasses);
+    expect(button).toHaveClass(
+      styles.button,
+      styles['button--primary'],
+      customClassName,
+    );
   });
 });
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,60 +1,24 @@
-import { twMerge } from 'tailwind-merge';
+import React from 'react';
+import clsx from 'clsx';
+import styles from './button.module.scss';
 
 export type Variant = 'primary' | 'neutral' | 'ghost';
-
-const variants: Record<Variant, string[]> = {
-  primary: [
-    'bg-primary',
-    'text-white',
-    'hover:bg-primary-600',
-    'active:bg-primary-700',
-    'transition-colors',
-    'duration-200',
-  ],
-  neutral: [
-    'bg-neutral-50',
-    'text-neutral',
-    'hover:bg-neutral-100',
-    'active:bg-neutral-200',
-    'transition-colors',
-    'duration-200',
-  ],
-  ghost: [
-    'bg-transparent',
-    'text-primary',
-    'hover:bg-primary-50',
-    'active:bg-primary-100',
-    'transition-colors',
-    'duration-200',
-  ],
-};
 
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   variant?: Variant;
 }
 
-export const getButtonClassName = ({
-  className,
-  variant = 'primary',
-}: Pick<ButtonProps, 'className' | 'variant'> = {}) => {
-  return twMerge([
-    'w-full',
-    'rounded-sm',
-    'px-6 py-4',
-    'font-medium',
-    variants[variant],
-    className,
-  ]);
-};
-
 export const Button = ({
   children,
   className,
-  variant,
+  variant = 'primary',
   ...props
 }: ButtonProps) => {
   return (
-    <button className={getButtonClassName({ className, variant })} {...props}>
+    <button
+      {...props}
+      className={clsx(styles.button, styles[`button--${variant}`], className)}
+    >
       {children}
     </button>
   );
