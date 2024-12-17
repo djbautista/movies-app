e4415915e7fc9979e9cf2df52771c921b4990bae
David Bautista <David.Bautista.-ND@disney.com>
2024-12-17 18:38:25 -0500

    refactor(scss-migration): Migrated header component

--- components/Header/Header.test.tsx
+++ components/Header/Header.test.tsx
@@ -1,9 +1,8 @@
-import { twMerge } from 'tailwind-merge';
 import { beforeEach, describe, expect, it } from 'vitest';
-
 import { cleanup, render, screen } from '@testing-library/react';
 
-import Header, { getHeaderClassName, HeaderProps } from './Header';
+import Header, { HeaderProps } from './Header';
+import styles from './header.module.scss';
 
 const setup = (props: HeaderProps = {}) =>
   render(<Header {...props} data-testid="header" />);
@@ -11,41 +10,22 @@ const setup = (props: HeaderProps = {}) =>
 describe('Header', () => {
   beforeEach(cleanup);
 
-  const baseClassNames = [
-    'flex',
-    'items-center',
-    'justify-between',
-    'bg-neutral-900',
-    'px-[18px] py-5',
-    'text-white',
-  ];
-
   it('renders Header component with the default props', () => {
-    const expectedClasses = [...baseClassNames];
-
     setup();
     const header = screen.getByTestId('header');
-    const generatedClasses = getHeaderClassName();
-
-    expect(generatedClasses).toBe(twMerge(expectedClasses));
-    expect(header.className).toBe(generatedClasses);
+    expect(header).toHaveClass(styles.header);
   });
 
   it('renders Header component with custom className', () => {
     const customClassName = 'custom-class';
-    const expectedClasses = [...baseClassNames, customClassName];
-
     setup({ className: customClassName });
-    const header = screen.getByTestId('header');
-    const generatedClasses = getHeaderClassName({ className: customClassName });
 
-    expect(generatedClasses).toBe(twMerge(expectedClasses));
-    expect(header.className).toBe(generatedClasses);
+    const header = screen.getByTestId('header');
+    expect(header).toHaveClass(styles.header, customClassName);
   });
 
   it('does not display back button when backHref is not provided', () => {
     setup();
-
     const backLink = screen.queryByRole('link');
     expect(backLink).not.toBeInTheDocument();
   });
@@ -55,8 +35,8 @@ describe('Header', () => {
 
     const backLink = screen.getByRole('link');
     expect(backLink).toBeInTheDocument();
-
     expect(backLink).toHaveAttribute('href', '/back');
+    expect(backLink).toHaveClass(styles['header__backLink']);
   });
 
   it('renders children as header title', () => {
@@ -65,5 +45,6 @@ describe('Header', () => {
 
     const headerTitle = screen.getByText(title);
     expect(headerTitle).toBeInTheDocument();
+    expect(headerTitle).toHaveClass(styles['header__title']);
   });
 });
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,26 +1,13 @@
 import { Button } from '@/components/Button';
 import Link from 'next/link';
+import clsx from 'clsx';
 
 import { MdArrowBack, MdOutlineMoreVert } from 'react-icons/md';
-import { twMerge } from 'tailwind-merge';
+import styles from './header.module.scss';
 
 export interface HeaderProps extends React.HTMLAttributes<HTMLHeadElement> {
   backHref?: string;
 }
-
-export const getHeaderClassName = ({
-  className,
-}: Pick<HeaderProps, 'className'> = {}) => {
-  return twMerge([
-    'flex',
-    'items-center',
-    'justify-between',
-    'bg-neutral-900',
-    'px-[18px] py-5',
-    'text-white',
-    className,
-  ]);
-};
 
 export const Header = ({
   backHref,
@@ -29,17 +16,21 @@ export const Header = ({
   ...props
 }: HeaderProps) => {
   return (
-    <header className={getHeaderClassName({ className })} {...props}>
-      <div className="flex items-center gap-4">
+    <header className={clsx(styles.header, className)} {...props}>
+      <div className={styles.header__left}>
         {backHref && (
-          <Link href={backHref} className="text-white">
-            <MdArrowBack className="h-6 w-6" />
+          <Link href={backHref} className={styles.header__backLink}>
+            <MdArrowBack className={styles.header__icon} />
           </Link>
         )}
-        <h1 className="text-xl font-bold">{children}</h1>
+        <h1 className={styles.header__title}>{children}</h1>
       </div>
-      <Button aria-label="More options" className="w-fit p-0" variant="ghost">
-        <MdOutlineMoreVert className="h-7 w-7 text-white" />
+      <Button
+        aria-label="More options"
+        className={styles.header__button}
+        variant="ghost"
+      >
+        <MdOutlineMoreVert className={styles.header__icon} />
       </Button>
     </header>
   );
