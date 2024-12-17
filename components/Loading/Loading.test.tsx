84916894a2638e993c90819cb5c538522d1ba68a
David Bautista <David.Bautista.-ND@disney.com>
2024-12-17 18:43:19 -0500

    refactor(scss-migration): Migrated loading component

--- components/Loading/Loading.test.tsx
+++ components/Loading/Loading.test.tsx
@@ -1,9 +1,8 @@
-import { twMerge } from 'tailwind-merge';
 import { beforeEach, describe, expect, it } from 'vitest';
-
 import { cleanup, render, screen } from '@testing-library/react';
 
-import Loading, { getLoadingClassName, LoadingProps } from './Loading';
+import Loading, { LoadingProps } from './Loading';
+import styles from './loading.module.scss';
 
 const setup = (props: LoadingProps = {}) =>
   render(<Loading {...props} data-testid="loading" />);
@@ -11,35 +10,18 @@ const setup = (props: LoadingProps = {}) =>
 describe('Loading', () => {
   beforeEach(cleanup);
 
-  const baseClassNames = [
-    'h-8 w-8',
-    'animate-spin',
-    'rounded-full',
-    'border-t-2 border-blue-500',
-  ];
-
   it('renders Loading component with the default props', () => {
-    const expectedClasses = [...baseClassNames];
-
     setup();
     const loading = screen.getByTestId('loading');
-    const generatedClasses = getLoadingClassName();
 
-    expect(generatedClasses).toBe(twMerge(expectedClasses));
-    expect(loading.className).toBe(generatedClasses);
+    expect(loading).toHaveClass(styles.loading);
   });
 
   it('renders Loading component with custom className', () => {
     const customClassName = 'custom-class';
-    const expectedClasses = [...baseClassNames, customClassName];
-
     setup({ className: customClassName });
     const loading = screen.getByTestId('loading');
-    const generatedClasses = getLoadingClassName({
-      className: customClassName,
-    });
 
-    expect(generatedClasses).toBe(twMerge(expectedClasses));
-    expect(loading.className).toBe(generatedClasses);
+    expect(loading).toHaveClass(styles.loading, customClassName);
   });
 });
--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -1,21 +1,12 @@
-import { twMerge } from 'tailwind-merge';
+import React from 'react';
+import clsx from 'clsx';
+
+import styles from './loading.module.scss';
 
 export type LoadingProps = React.HTMLAttributes<HTMLDivElement>;
 
-export const getLoadingClassName = ({
-  className,
-}: Pick<LoadingProps, 'className'> = {}) => {
-  return twMerge([
-    'h-8 w-8',
-    'animate-spin',
-    'rounded-full',
-    'border-t-2 border-blue-500',
-    className,
-  ]);
-};
-
 export const Loading = ({ className, ...props }: LoadingProps) => {
-  return <div className={getLoadingClassName({ className })} {...props} />;
+  return <div className={clsx(styles.loading, className)} {...props} />;
 };
 
 export default Loading;
